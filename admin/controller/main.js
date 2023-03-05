be42a9bbbe5c841d239f884b8764a420fb68e94d
congvinh <congvinhnguyennn@gmail.com>
2023-03-06 00:34:12 +0700

    Link admin to customer page

--- admin/controller/main.js
+++ admin/controller/main.js
@@ -1,6 +1,5 @@
 getProductFromAPI()
 
-
 function getProductFromAPI(){
     apiProducts().then((response) => {
         const products = response.data.map((product) => {
@@ -10,6 +9,7 @@ function getProductFromAPI(){
     }).catch((error) =>{
         alert("Thất bại");
     });
+    
 }
 
 function renderProduct(products){
@@ -26,13 +26,70 @@ function renderProduct(products){
                     <td>${product.type}</td>
                     <td>
                         <button class="btn btn-danger" onclick="deleteProduct('${product.id}')">Xóa</button>
-                        <button class="btn btn-primary" onclick="selectProduct(${product.id})" data-target="#myModal" data-toggle="modal">Sửa</button>
+                        <button class="btn btn-info" onclick="selectProduct('${product.id}')" data-target="#myModal" data-toggle="modal">Sửa</button>
                     </td>
                 </tr>
             `
         );
     }, "");
     getEle("#tableDanhSach").innerHTML = html;
+}
+
+function checkValidateForm(){
+        isValid = true;
+        let name = getEle("#name").value,
+            price = getEle("#price").value,
+            photo = getEle("#photo").value,
+            desc = getEle("#desc").value,
+            type = getEle("#type").value;
+    
+        // Name
+        if(name === ""){
+            isValid = false;
+            getEle("#name").style.borderColor = "red";
+            getEle("#tbName").innerHTML = "Không được để trống";
+            getEle("#tbName").style.display = "block";
+        }
+    
+        // Price
+        if(price === ""){
+            isValid = false;
+            getEle("#price").style.borderColor = "red";
+            getEle("#tbPrice").innerHTML = "Không được để trống";
+            getEle("#tbPrice").style.display = "block";
+    
+        }else if(!isNumber(price)){
+            isValid = false;
+            getEle("#price").style.borderColor = "red";
+            getEle("#tbPrice").innerHTML = "Giá tiền phải là số";
+            getEle("#tbPrice").style.display = "block";
+        }
+    
+        // Photo
+        if(photo === ""){
+            isValid = false;
+            getEle("#photo").style.borderColor = "red";
+            getEle("#tbPhoto").innerHTML = "Không được để trống";
+            getEle("#tbPhoto").style.display = "block";
+        }
+    
+        // Desc
+        if(desc === ""){
+            isValid = false;
+            getEle("#desc").style.borderColor = "red";
+            getEle("#tbDesc").innerHTML = "Không được để trống";
+            getEle("#tbDesc").style.display = "block";
+        }
+    
+        // Type
+        if(type === "Select brand"){
+            isValid = false;
+            getEle("#type").style.borderColor = "red";
+            getEle("#tbType").innerHTML = "Không được để trống";
+            getEle("#tbType").style.display = "block";
+        }
+    
+        return isValid;
 }
 
 function addProduct(){
@@ -43,20 +100,21 @@ function addProduct(){
         desc: getEle("#desc").value,
         type: getEle("#type").value,
     }
-    apiAddProducts(product).then((response) => {
+
+    checkValidateForm() ? apiAddProducts(product).then(response => {
         getProductFromAPI();
-    }).catch((error) => {
-        alert("Thất bại");
-    });
+        alert("Thêm sản phẩm thành công");
+    }).catch(error => {
+        alert("Thêm sản phẩm thất bại");
+    }) : alert("Thông tin sản phẩm không được để trống");
 }
-
-
 
 function deleteProduct(productId){
     apiDeleteProduct(productId).then(() => {
         getProductFromAPI();
+        alert("Xóa sản phẩm thành công");
     }).catch((error) => {
-        alert("Thất bại");
+        alert("Xóa sản phẩm thất bại");
     });
 }
 
@@ -70,12 +128,12 @@ function selectProduct(productId){
         getEle("#desc").value = product.desc;
         getEle("#type").value = product.type;
     }).catch((error) => {
-        alert("Thất bại");
+        alert("Chọn sản phẩm thất bại");
     })
 
     getEle("#header-title").innerHTML = "Sửa thông tin sản phẩm";
     getEle(".modal-footer").innerHTML = `
-    <button class="btn btn-primary" onclick="updateProduct()">Cập nhật</button>
+    <button class="btn btn-primary" onclick="updateProduct('${productId}')">Cập nhật</button>
     <button class="btn btn-danger" data-dismiss="modal">Đóng</button>
     `
 }
@@ -92,8 +150,9 @@ function updateProduct(productId) {
 
     apiUpdateProduct(productId, product).then((response) => {
         getProductFromAPI();
+        alert("Cập nhật thành công")
     }).catch(() => {
-        alert('Thất bại');
+        alert('Cập nhật thất bại');
     });
 }
 
@@ -101,7 +160,7 @@ function updateProduct(productId) {
 getEle("#btnThem").addEventListener("click", () => {
     getEle("#header-title").innerHTML = "Thêm sản phẩm";
     getEle(".modal-footer").innerHTML = `
-        <button class="btn btn-primary" onclick="addProduct()">Thêm sản phẩm</button>
+        <button class="btn btn-primary" onclick="addProduct()">Thêm</button>
         <button class="btn btn-secondary" data-dismiss="modal">Huỷ</button>
     `;
 
@@ -111,6 +170,71 @@ getEle("#btnThem").addEventListener("click", () => {
     getEle("#desc").value = "";
     getEle("#type").value = "Select brand";
   });
+
+// ============ Tìm kiếm sản phẩm ===============   
+getEle("#searchName").addEventListener("keyup", () => {
+    const searchValue = getEle("#searchName").value;
+    apiProducts(searchValue).then((response) => {
+        const products = response.data.map((product) => {
+            return new Product(product.id, product.name, product.price, product.photo, product.desc, product.type);
+        });
+        renderProduct(products);
+    }).catch((error) => {
+        alert("Thất bại");
+    });
+});
+
+// Sorting Product Price
+// let isAscending = false;
+ 
+// getEle("#sortValue").addEventListener("click", () => {
+//     if(isAscending){
+//         getEle("#SapXepGiam1").style.display = "none";
+//     } else {
+//         getEle("#SapXepGiam1").style.display = "inline-block";
+//     }
+    
+//     if(!isAscending){
+//         getEle("#SapXepTang1").style.display = "none";
+//     } else {
+//         getEle("#SapXepTang1").style.display = "inline-block";
+//     }
+
+//     var sortValue = getEle("#sortValue").value;
+//     apiProducts(null, sortValue).then((response) => {
+//         var products = response.data.sort((a, b) => (isAscending ? a.price - b.price : b.price - a.price)) //ternary operator using variable determined earlier
+//             .map((product) => {
+//                 return new Product(product.id, product.name, product.price, product.photo, product.desc, product.type);
+//             });
+//         renderProduct(products);
+//         isAscending = !isAscending;           //update the value of isAscending after each call
+//     }).catch((error) => {
+//         alert("Thất bại");
+//     });
+// });
+
+
+
+
+getEle("#sortValue").addEventListener("click", () => {
+    const isDescending = getEle("#SapXepGiam1").style.display === "none"; 
+    getEle("#SapXepGiam1").style.display = isDescending ? "inline-block" : "none"; 
+    getEle("#SapXepTang1").style.display = !isDescending ? "inline-block" : "none";
+
+    var sortValue = getEle("#sortValue").value;
+    apiProducts(null, sortValue).then((response) => {
+        var products = response.data.sort((a, b) => (isDescending ? b.price - a.price : a.price - b.price))
+            .map((product) => {
+                return new Product(product.id, product.name, product.price, product.photo, product.desc, product.type);
+            });
+        renderProduct(products);
+    }).catch((error) => {
+        alert("Thất bại");
+    });
+});
+
+
+
 
 // Helper
 function getEle(selector){
